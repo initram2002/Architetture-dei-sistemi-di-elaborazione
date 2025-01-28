2a075f697cb91900dcdcf6edddd267ae52013e2d
Michela Martini <martini.michela02@gmail.com>
2025-01-28 13:28:58 +0100

    first commit of third example "first attemp"

---
+++ Codice ed esempi/3 - First attempt/first_attempt.s
@@ -0,0 +1,8 @@
+; Architetture dei Sistemi di Elaborazione - 02GOLOV ;
+; author:       Michela MARTINI - Politecnico di Torino  ;
+; creation:     28 January 2025                          ;
+; last update:  28 January 2025                          ;
+
+    .data
+v1: .byte   0x1, 0xea
+v2: .word   0xc1a0c1a0bac1bac1
