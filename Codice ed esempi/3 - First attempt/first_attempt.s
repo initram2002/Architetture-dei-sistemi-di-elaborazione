9b8f5f232f3007078671ee8ada6f0bab193269e8
Michela Martini <martini.michela02@gmail.com>
2025-01-31 09:44:32 +0100

    correction header program

--- Codice ed esempi/3 - First attempt/first_attempt.s
+++ Codice ed esempi/3 - First attempt/first_attempt.s
@@ -1,7 +1,7 @@
-; Architetture dei Sistemi di Elaborazione - 02GOLOV ;
+; Architetture dei Sistemi di Elaborazione - 02GOLOV     ;
 ; author:       Michela MARTINI - Politecnico di Torino  ;
 ; creation:     28 January 2025                          ;
-; last update:  28 January 2025                          ;
+; last update:  31 January 2025                          ;
 
     .data
 v1: .byte       0x1, 0xea
